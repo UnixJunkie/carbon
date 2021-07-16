b194486e2d931fe4aa25f66270d78daff1052da1
Francois Berenger <unixjunkie@sdf.org>
2021-07-16 16:20:13 +0900

    Carbon.string_of_*

--- src/carbon.ml
+++ src/carbon.ml
@@ -27,6 +27,18 @@ let aliphatic_orga_of_string = function
   | "I"   -> I
   | other -> raise (Invalid_aliphatic_orga other)
 
+let string_of_aliphatic_orga = function
+  | B  -> "B"
+  | C  -> "C"
+  | N  -> "N"
+  | O  -> "O"
+  | S  -> "S"
+  | P  -> "P"
+  | F  -> "F"
+  | Cl -> "Cl"
+  | Br -> "Br"
+  | I  -> "I"
+
 type aromatic_orga = Aro_B
                    | Aro_C
                    | Aro_N
@@ -45,10 +57,24 @@ let aromatic_orga_of_string = function
   | "p" -> Aro_P
   | other -> raise (Invalid_aromatic_orga other)
 
+let string_of_aromatic_orga = function
+  | Aro_B -> "b"
+  | Aro_C -> "c"
+  | Aro_N -> "n"
+  | Aro_O -> "o"
+  | Aro_S -> "s"
+  | Aro_P -> "p"
+
 type atom = Bracket_atom of string
-          | Aliphatic_organic of aliphatic_orga
-          | Aromatic_organic of aromatic_orga
+          | Aliphatic_orga of aliphatic_orga
+          | Aromatic_orga of aromatic_orga
           | Star
+
+let string_of_atom = function
+  | Bracket_atom ba -> ba
+  | Aliphatic_orga ali -> string_of_aliphatic_orga ali
+  | Aromatic_orga aro -> string_of_aromatic_orga aro
+  | Star -> "*"
 
 type bond = Single
           | Double
@@ -69,6 +95,15 @@ let bond_of_string = function
   | "/"  -> Up
   | "\\" -> Down
   | other -> raise (Invalid_bond other)
+
+let string_of_bond = function
+  | Single    -> "-"
+  | Double    -> "="
+  | Triple    -> "#"
+  | Quadruple -> "$"
+  | Aromatic  -> ":"
+  | Up        -> "/"
+  | Down      -> "\\"
 
 type ringbond = Ring_opening of int
               | Ring_closure of int
